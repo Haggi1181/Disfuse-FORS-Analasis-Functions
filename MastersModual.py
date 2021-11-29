c00ad3ae181fa3c1571e1ad801daa5517bc86d3c
Haggi1181 <yrrahtac2@gmail.com>
2021-11-29 14:32:03 +0000

    Added Running Peek Save to debug

--- MastersModual.py
+++ MastersModual.py
@@ -620,7 +620,11 @@ def PeekFinder(arrSpectralX, arrSpectralY, debug = False):
         i=i+1.0
     #check to see if the found peek is close enough to the max reflectivity of the overall spectra to be considerd the leveling off point
     if funInterpFunction(runningpeek) <= max(arrSmoothedY)-0.2 and runningpeek <= 601.0:
+        f.write("RunningPeek Not Saved \n")
+        f.write("Running Peek Val:" + str(runningpeek))
         pass
     else:
+        f.Write("RunningPeek Saved \n")
+        f.write("Running Peek Val:" + str(runningpeek))
         arr0.append(runningpeek)
     return(arr0)
